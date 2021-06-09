e2b24da7eb2e87bd94a27dcb03a5ea1d11ac6ad7
CORP\kumarv124 <vivek.kumar17@rsa.com>
2021-06-09 18:53:09 +0530

    first commit

---
+++ src/main/java/com/vivek/oauthgithub/OauthGithubApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.oauthgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthGithubApplication.class, args);
+	}
+
+}
